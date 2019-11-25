8cc84351e3b5ff7d423f1c33ec8c73520cbf246b
Osivwi Okiti <osivwiokiti@gmail.com>
2019-11-25 20:32:34 +0100

    UPDATE configure jest to collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  // An array of glob patterns indicating a set of files for which coverage information
+  // should be collected
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js',
+    '!**.spec.js',
+    '!**/node_modules/**',
+  ],
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
 };
